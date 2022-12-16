28d2513ec069f5d90f0ac505f43fbb66be809532
Ekaterina <kmatway@yandex.ru>
2022-12-16 22:11:16 +0500

    решение задачи 13

---
+++ Z13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//что третьей цифры нет.
+
+Console.WriteLine("Введите число n = ");
+int n = int.Parse(Console.ReadLine());
+
+if (n > 99)
+{
+    while (n >= 1000)
+    {
+        n = n/10;
+    }
+    int result = n%10;
+    Console.WriteLine("Третья цифра числа =" + result);
+}
+else
+{
+ Console.WriteLine("Третьей цифры нет");
+}
